f5a96d01f7322bff50d5803dfbe00ca150a53c02
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-10-04 17:07:19 -0400

    nav hamburger functionality in progress

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import { GiHamburgerMenu } from "react-icons/gi";
-
+import '../styles/fonts.css'
 export default function Nav() {
   return (
-    <div className='flex bg-[#191b1e] h-24 text-white flex-row items-center fixed w-full mx-auto px-6 justify-between '>
-      <a className='font-bold w-full text-2xl' href='/'>WESTPORT CONVINIANCE</a>
+    <div>
+    <div className='flex bg-[#191b1e] h-24 text-white flex-row items-center w-full mx-auto px-6 justify-between '>
+      <a className='font-bold text-5xl text-green-600 font-[cool] md:text-6xl' href='/'>WESTPORT CONVINIANCE</a>
       <div className='hidden md:flex' >
       <a className='p-4' href="/">Home</a>
       <a className='p-4' href="/gallery">Gallery</a>
-      <a  className='p-4' href="/">Home</a>
+      <a  className='p-4' href="/">About</a>
       </div>  
-      <button className='ml-4'><GiHamburgerMenu size={30}></GiHamburgerMenu></button>
+      <button className='md:hidden'><GiHamburgerMenu size={50}></GiHamburgerMenu></button>
+    </div>  
+    <div className='text-black bg-gray-300 h-72 md:hidden'>
+     <a className='p-4' href="/">Home</a>
+      <a className='p-4' href="/gallery">Gallery</a>
+      <a  className='p-4' href="/">About</a>
     </div>
+    </div>  
   )
 }
