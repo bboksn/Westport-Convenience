877c6a4c85dc9a1f747c167a5a4036b16e9f7107
Ben <bboksner@LAPTOP-0U0LFUE0.localdomain>
2022-10-08 22:12:51 -0400

    added yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), image()]
+  integrations: [react(), tailwind(), image()],
+  
 });
